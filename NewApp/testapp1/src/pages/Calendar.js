95e8fc04a4d6348bedf45cd62bf79f4c7f31e29a
StevenPhang22 <StevenPhang22@users.noreply.github.com>
2022-03-29 13:38:23 -0400

    added basic style

--- NewApp/testapp1/src/pages/Calendar.js
+++ NewApp/testapp1/src/pages/Calendar.js
@@ -2,9 +2,25 @@ import React from "react";
 
 class Calendar extends React.Component  {
     render() {
+        const headerStyle = {
+            color: "white",
+            backgroundColor: "DodgerBlue",
+            padding: "10px",
+            fontFamily: "Arial",
+            textAlign: "center"
+          };
+        const calendarStyle = {
+            backgroundColor: "orange",
+            padding: "10px"
+          };
       return(         
-        <div>          
-          <iframe title= "calendar" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=cy5waGFuZzIyQGdtYWlsLmNvbQ&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%230B8043" height="500" width="500"/>         
+        <div>
+        <body>
+           <h1 style={headerStyle}>Meeting Times</h1>
+           <p style={{ textAlign: 'center' }}><b>Tuesdays and Thursdays at 5:30 pm at the Solar Park</b></p>
+           <br></br>        
+          <center><iframe style={calendarStyle} title= "calendar" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=cy5waGFuZzIyQGdtYWlsLmNvbQ&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%230B8043" height="500" width="500"/></center>
+         </body>         
         </div>
       );
     }
