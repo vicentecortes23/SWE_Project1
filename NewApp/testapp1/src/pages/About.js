b73661bb57d253be7efe3a87dbdc35e10cb04057
JDdDee <jdchen333@gmail.com>
2022-03-29 13:15:29 -0400

    testing page and button added

---
+++ NewApp/testapp1/src/pages/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function About() {
+
+    class LinkButton extends React.Component {
+
+        render() {
+          return (
+            <button className="linkbutton">
+            
+              
+            </button>
+          );
+        }
+      }
+
+    return(<div>
+         <h2>The aggregator website is about robotics</h2>
+                <a href= "https://reactjs.org/tutorial/tutorial.html"><button>linkbutton</button> Visitsomething </a>
+        
+    </div>)
+}
+
+export default About;
