9f144cf08143cab222b86110d97a7a3da94befee
vicentecortes23 <vicentecortes@ufl.edu>
2022-03-29 13:28:22 -0400

    Added log out and other func

--- NewApp/testapp1/src/pages/Login.js
+++ NewApp/testapp1/src/pages/Login.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from 'firebase/auth';
+import {useNavigate} from "react-router-dom";
 
 function Login({setIsAuth}) {
+    let navigate = useNavigate();
+
     const signInWithGoogle = ( )  => {
         signInWithPopup(auth,provider).then((result) => {
+           //stay logged in if you close page
            localStorage.setItem("isAuth", true);
-           setIsAuth = true; 
+           setIsAuth = true;
+         navigate("/");
         })
 
     };
