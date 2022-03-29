076ebec0f202cae2e2b969ead4bb20e8715c6606
StevenPhang22 <StevenPhang22@users.noreply.github.com>
2022-03-29 12:48:12 -0400

    refactored calendar to be a class

--- NewApp/testapp1/src/pages/Calendar.js
+++ NewApp/testapp1/src/pages/Calendar.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function Calendar() {
-
-    return (<div>
-         <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=cy5waGFuZzIyQGdtYWlsLmNvbQ&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%230B8043" height="500" width="500"/>
-    </div>
-    )
-}
+class Calendar extends React.Component  {
+    render() {
+      return(         
+        <div>          
+          <iframe title= "calendar" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=cy5waGFuZzIyQGdtYWlsLmNvbQ&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%230B8043" height="500" width="500"/>         
+        </div>
+      );
+    }
+  }
 
 export default Calendar;
