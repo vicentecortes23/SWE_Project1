7948fac81bf58d5924296287d4cf7384a391a5b5
vicentecortes23 <vicentecortes@ufl.edu>
2022-03-29 13:28:22 -0400

    Added log out and other func

--- NewApp/testapp1/src/App.js
+++ NewApp/testapp1/src/App.js
@@ -5,15 +5,26 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Calendar from './pages/Calendar';
 import { useState } from "react";
+import {signOut} from "firebase/auth";
+import {auth} from "./firebase-config"
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
+
+  const signOutUser = () => {
+    signOut(auth).then(() => {
+      localStorage.clear()
+      setIsAuth(false)
+      window.location.pathname = "/login";
+    });
+  };
+
   return (
     <Router>
       <nav>
         <Link to="/"> Home </Link>
         <Link to="/Calendar"> Calendar </Link>
-        <Link to="/login"> Login </Link>
+        {!isAuth ? <Link to="/login"> Login </Link> : <button> Log Out </button>}
       </nav>
       <Routes>
         <Route path="/" element ={<Home />} />
