3f13bc90fbfdf51e4cc24a313c332b920418b032
JDdDee <jdchen333@gmail.com>
2022-04-01 13:41:56 -0400

    z

---
+++ NewApp/testapp1/src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Contact() {
+    return(<div>
+        <h1> TestLink </h1>
+        <a href= "https://reactjs.org/tutorial/tutorial.html">
+                  <button>linkbutton</button> Visitsomething 
+        </a>
+    </div>
+    ) 
+}
+
+export default Contact;
