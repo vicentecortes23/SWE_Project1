662f7f8c85e4f5cc06aa9b2fa2f585c25c8a3fad
JDdDee <jdchen333@gmail.com>
2022-03-31 20:29:15 -0400

    added a wider banner image to test resolutions

--- NewApp/testapp1/src/pages/About.js
+++ NewApp/testapp1/src/pages/About.js
@@ -6,6 +6,8 @@ function About() {
 
     class LinkButton extends React.Component {
 
+      
+
         render() {
           return (
 
@@ -18,14 +20,14 @@ function About() {
       }
 
     return(<div>
-        
+        <WebImage />
 
          <h2>The aggregator website is about robotics</h2>
                 <a href= "https://reactjs.org/tutorial/tutorial.html">
                   <button>linkbutton</button> Visitsomething 
                 </a>
 
-          <WebImage />
+          
         
     </div>)
 }
