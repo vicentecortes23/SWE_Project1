eb159f9c96f9aa347515f87c083b69ffdfd54e7f
Zclaussen <58396431+Zclaussen@users.noreply.github.com>
2022-03-31 22:46:11 -0400

    3/31/2022
    Home page image
    About page dropdown

--- NewApp/testapp1/src/pages/About.js
+++ NewApp/testapp1/src/pages/About.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function About() {
+class About extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      showMenu: false,
+    };
+    
+    this.showMenu = this.showMenu.bind(this);
+  }
+  
+  showMenu(event) {
+    event.preventDefault();
+    
+    this.setState(
+      {
+        showMenu: true,
+      });
+}
 
-    class LinkButton extends React.Component {
-
-        render() {
-          return (
-            <button className="linkbutton">
-            
-              
-            </button>
-          );
-        }
-      }
-
-    return(<div>
-         <h2>The aggregator website is about robotics</h2>
-                <a href= "https://reactjs.org/tutorial/tutorial.html"><button>linkbutton</button> Visitsomething </a>
+  render() {
+    return (
+      <div>
+        <button onClick={this.showMenu}>
+          About Us
+        </button>
         
-    </div>)
+        {
+          this.state.showMenu
+            ? (
+              <div className="menu">
+                <button> 
+                    Description: Aggregator is a collaborative project that competes in the NASA Lunabotics Competition. The project consists of the design, development, 
+                    and construction of an autonomous mobile robot capable of completing objectives highlighted in the Lunabotics Competition rules of the year. Aggregator’s 
+                    main objective is to connect bright engineers, both professional and novice, to facilitate a productive learning experience and conquer engineering problems. 
+                    We accept anyone with drive and passion for engineering. Through Aggregator, one can expect to learn how to create robust mechanical design, use SolidWorks, 
+                    manufacture, develop microcontrollers, code C++, and use ROS. 
+                  </button>
+                  <button>
+                    Meetings: We typically meet Tuesdays, Thursdays, or Saturdays depending on the sub-team you wish to participate in (there are mechanical and electrical sub teams).
+                    To get the most up-to-date information about when we meet, visit our discord.
+                  </button>
+                  <button>
+                    Contact Info: Our leadership changes annually, so it is best, and easiest, to contact us directly in the discord. You can do so by asking questions in the
+                     “questions” channel or direct messaging anyone that is a “Lead” or an “Executive Board” member.
+                  </button>
+              </div>
+            )
+            : (
+              null
+            )
+        }
+      </div>
+    );
+  }
 }
 
 export default About;
