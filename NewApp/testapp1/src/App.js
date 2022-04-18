23a27b00704f265db3b5ae21ad8fa5e3850da9f6
vicentecortes23 <vicentecortes@ufl.edu>
2022-04-18 17:08:19 -0400

    Making Changes!

--- NewApp/testapp1/src/App.js
+++ NewApp/testapp1/src/App.js
@@ -11,7 +11,8 @@ import {signOut} from "firebase/auth";
 import {auth} from "./firebase-config"
 import React  from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {provider } from "./firebase-config";
+import { signInWithPopup } from "firebase/auth";
 
 
 function App() {
@@ -21,29 +22,40 @@ function App() {
     signOut(auth).then(() => {
       localStorage.clear()
       setIsAuth(false)
-      window.location.pathname = "/login";
+    });
+  };
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
     });
   };
 
   return (
-    <Router>
-      <nav>
-        <Link to="/"> Home </Link>
-        <Link to="/Calendar"> Calendar </Link>
-        <Link to="/About"> About </Link>
-        <Link to="/Contact"> Contact </Link>
-        {!isAuth ? 
-          (<Link to="/login"> Login </Link>) 
-          : (<button onClick={signOutUser}> Log Out </button>)
-        }
-      </nav>
-      <Routes>
-        <Route path="/" element ={<Home />} />
-        <Route path="/calendar" element ={<Calendar />} />
-        <Route path="/login" element ={<Login setIsAuth={setIsAuth} />} />
-        <Route path="/About" element ={<About/>} />
-        <Route path="/Contact" element = {<Contact/>} />
-      </Routes></Router>
+      <Router>
+        <nav>
+          <Link to="/"> Home </Link>
+          <Link to="/Calendar"> Calendar </Link>
+          <Link to="/About"> About </Link>
+          <Link to="/Contact"> Contact </Link>
+          {!isAuth ? 
+            (<button className="btn btn-info" onClick={signInWithGoogle}>
+            Sign in with Google
+          </button>) 
+            : (<button className="btn btn-info" onClick={signOutUser}> Log Out </button>)
+          }
+        </nav>
+        <Routes>
+          <Route path="/" element ={<Home />} />
+          <Route path="/calendar" element ={<Calendar />} />
+          <Route path="/login" element ={<Login setIsAuth={setIsAuth} />} />
+          <Route path="/About" element ={<About/>} />
+          <Route path="/Contact" element = {<Contact/>} />
+        </Routes>
+        <div class="p-3 mb-2 bg-light text-dark">
+        </div>
+        </Router>
   );
 }
 
--- NewApp/testapp1/src/pages/Home.js
+++ NewApp/testapp1/src/pages/Home.js
@@ -1,15 +1,21 @@
 import React from "react"
 import pic from "./images/test.jpg"
 import ContactUs from './ContactUs';
+import './Home.css';
+ 
 
 function Home() {
     return (
-        <div>   
+        <div className="body">   
             <center>
                 <img src ={pic} />
             </center>  
             <div>
-                <ContactUs/>
+            <center>
+                    <h1>Contact Us!</h1>
+                    <h2>Please send us an email if you're interested in joining the club, have any recommendations, or just want to get in touch! </h2>
+                    <ContactUs/>
+                    </center>
             </div>
         </div>
     )
