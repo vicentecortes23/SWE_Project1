ec29d5ddd089a133c5dd154d57cb8e17cb426b31
vicentecortes23 <vicentecortes@ufl.edu>
2022-04-18 20:06:29 -0400

    push again

--- NewApp/testapp1/src/App.js
+++ NewApp/testapp1/src/App.js
@@ -37,31 +37,31 @@ function App() {
 
   return (
     <div>
-    <Router>
-      <nav>
-        <Link to="/"> Home </Link>
-        <Link to="/Calendar"> Calendar </Link>
-        <Link to="/About"> About </Link>
-        <Link to="/Contact"> Contact </Link>
-        {!isAuth ? 
-          ( 
-          <button className="btn btn-info" onClick={signInWithGoogle}>
-            Sign in with Google
-          </button>) 
-          : (<button className="btn btn-info" onClick={signOutUser}> Log Out </button>)
-        }
-      </nav>
-      <Routes>
-        <Route path="/" element ={<Home />} />
-        <Route path="/calendar" element ={<Calendar />} />
-        <Route path="/login" element ={<Login setIsAuth={setIsAuth} />} />
-        <Route path="/About" element ={<About/>} />
-        <Route path="/Contact" element = {<Contact/>} />
-      </Routes>
-    </Router>
-    <div>
-        <Footer/>
-    </div>
+      <Router>
+        <nav>
+          <Link to="/"> Home </Link>
+          <Link to="/Calendar"> Calendar </Link>
+          <Link to="/About"> About </Link>
+          <Link to="/Contact"> Contact </Link>
+          {!isAuth ? 
+            ( 
+            <button className="btn btn-info" onClick={signInWithGoogle}>
+              Sign in with Google
+            </button>) 
+            : (<button className="btn btn-info" onClick={signOutUser}> Log Out </button>)
+          }
+        </nav>
+        <Routes>
+          <Route path="/" element ={<Home />} />
+          <Route path="/calendar" element ={<Calendar />} />
+          <Route path="/login" element ={<Login setIsAuth={setIsAuth} />} />
+          <Route path="/About" element ={<About/>} />
+          <Route path="/Contact" element = {<Contact/>} />
+        </Routes>
+      </Router>
+      <div>
+          <Footer/>
+      </div>
   </div>
   );
 }
